5b20283c466ea87f1347d37832ea06e14efc8d9d
DarkJoker360 <simoespo159@gmail.com>
2021-12-22 11:58:39 +0100

    fajita: Exclude updater
    * As I am doing personal builds this will
      prevent official updates to be tracked.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oneplus \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Inherit from oneplus sdm845-common
 $(call inherit-product, device/oneplus/sdm845-common/common.mk)
 
